1fd90a635a0665a39667f57534ce30f890fbef74
alxcay <103696780+alxcay@users.noreply.github.com>
2022-12-15 10:49:00 -0500

    Major Updates

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ McButton.click( function() {
     McBar3.velocity({ top: "50%" }, {duration: 200, easing: "swing"})
     			.velocity({rotateZ:"90deg"}, {duration: 800, delay: 200, easing: [500,20] });
     McButton.velocity({rotateZ:"135deg"}, {duration: 800, delay: 200, easing: [500,20] });
+    
   } else {
     McButton.velocity("reverse");
 		McBar3.velocity({rotateZ:"0deg"}, {duration: 800, easing: [500,20] })
@@ -24,19 +25,19 @@ McButton.click( function() {
 
 
 
-/*$element.velocity({ 
-    width: "500px",
-    property2: value2
-}, {
-    duration: 400,
-    easing: "swing",
-    queue: "",
-    begin: undefined,
-    progress: undefined,
-    complete: undefined,
-    display: undefined,
-    visibility: undefined,
-    loop: false,
-    delay: false,
-    mobileHA: true
-});*/
+// $element.velocity({ 
+//     width: "500px",
+//     property2: value2
+// }, {
+//     duration: 400,
+//     easing: "swing",
+//     queue: "",
+//     begin: undefined,
+//     progress: undefined,
+//     complete: undefined,
+//     display: undefined,
+//     visibility: undefined,
+//     loop: false,
+//     delay: false,
+//     mobileHA: true
+// });
